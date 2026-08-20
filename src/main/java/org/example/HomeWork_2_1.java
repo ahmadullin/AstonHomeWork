5ed6140370ec873bf9232c9db601f3c4e1ce3564
Asia <marvanovaasia@gmail.com>
2026-08-20 14:22:38 +0500

    add refresh branch HomeWork_2.1

---
+++ src/main/java/org/example/HomeWork_2_1.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class HomeWork_2_1 {
+    public static void firstHW() {
+        System.out.println("It's first Home Work!");
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-
+        HomeWork_2_1.firstHW();
     }
 }
